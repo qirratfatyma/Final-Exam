0a37159f3287530414b9ea2f878d1df0b20fcfa1
Syeda Qirrat Fatima <syedaqirratfatima@gmail.com>
2023-12-20 16:33:39 +0500

    Changes Made

--- src/App.js
+++ src/App.js
@@ -3,8 +3,6 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
-import Future from "./components/future";
-
 
 function App() {
   return (
@@ -13,7 +11,6 @@ function App() {
       <Routes>
         <Route path="/" element={<HeroSection />} />
         <Route path="/HeroSection" element={<HeroSection />} />
-        <Route path="/future" element={<Future />}/>
       </Routes>
       <Footer />
     </div>
