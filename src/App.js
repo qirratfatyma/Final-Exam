71b423e87267700249621e6b78e5984e7a5c1430
Syeda Qirrat Fatima <syedaqirratfatima@gmail.com>
2023-12-20 14:41:17 +0500

    Commit Two

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
-import Contact from "./components/Contact";
 import Skills from "./components/Skills";
-import Documentation from "./components/Documentation";
-import Signup from "./components/Signup";
-import Login from "./components/Login";
+
 
 function App() {
   return (
@@ -16,11 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HeroSection />} />
         <Route path="/Skills" element={<Skills />} />
-        <Route path="/Contact" element={<Contact />} />
-        <Route path="/Documentation" element={<Documentation />} />
-        <Route path="/Signup" element={<Signup />} />
         <Route path="/HeroSection" element={<HeroSection />} />
-        <Route path="/login" element={<Login />} />
       </Routes>
       <Footer />
     </div>
--- src/components/Documentation.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Documentation() {
-  return <div>Documentation section</div>;
-}
-
-export default Documentation;
